ed95a9614c3862346eec1c439b3a01a96135aed6
liou <liou666@126.com>
2024-03-07 22:05:32 +0800

    chore: base config

--- src/types.d.ts
+++ src/types.d.ts
@@ -24,3 +24,27 @@ export interface MetaDataTwitter {
   site?: string;
   cardType?: string;
 }
+
+export interface TTSConfig {
+  language: string // tts stt
+  voiceName: string // 参考 https://aka.ms/speech/tts-languages
+  rate: number // 语速
+  voiceStyle: string // 情感
+  volume: number // 音量
+  pitch: number // 音高
+}
+
+export enum AudioType{
+  MP3 = 'audio/mp3',
+  WAV = 'audio/wav',
+  OGG = 'audio/ogg',
+  WEBM = 'audio/webm',
+}
+
+
+export enum AudioFileSuffix{
+  MP3 = '.mp3',
+  WAV = '.wav',
+  OGG = '.ogg',
+  WEBM = '.webm',
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,7 @@ export default {
   darkMode: 'class',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   plugins: [iconsPlugin({
-    collections: getIconCollections(['mdi', 'ic', 'carbon']),
+    collections: getIconCollections(['mdi', 'ic', 'carbon', 'eos-icons', 'material-symbols']),
   })],
   theme: {
     extend: {
@@ -15,7 +15,20 @@ export default {
         accent: 'var(--c-accent)',
         default: 'var(--c-text-default)',
         muted: 'var(--c-text-muted)',
+        border: {
+          default: 'var(--c-border-base)',
+        },
       },
+      backgroundColor: {
+        'page': 'var(--c-bg-page)',
+        'page-dark': 'var(--c-bg-page-dark)',
+        'active': 'var(--c-accent)',
+      },
+      textColor: {
+        page: 'var(--c-text-page)',
+        muted: 'var(--c-text-muted)',
+      },
+
     },
   },
 }
